edc4f07338686e6a8af84399df45fc1c6648ece2
SahibMobDev <sahibmobdev@gmail.com>
2023-07-28 20:58:55 +0400

    Uploading Data - Firebase Firestore

--- app/src/main/java/com/github/sahibmobdev/firebasetutorial/MainActivity.kt
+++ app/src/main/java/com/github/sahibmobdev/firebasetutorial/MainActivity.kt
@@ -2,10 +2,47 @@ package com.github.sahibmobdev.firebasetutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.github.sahibmobdev.firebasetutorial.databinding.ActivityMainBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    private val personCollectionRef = Firebase.firestore.collection("person")
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnUploadData.setOnClickListener {
+            binding.apply {
+                val firstName = etFirstName.text.toString()
+                val lastName = etLastName.text.toString()
+                val age = etAge.text.toString().toInt()
+                val person = Person(firstName, lastName, age)
+                savePerson(person)
+            }
+        }
+
+    }
+
+    private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            personCollectionRef.add(person).await()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, "Successfully saved data.", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: Exception) {
+           withContext(Dispatchers.Main) {
+               Toast.makeText(this@MainActivity, e.message.toString(), Toast.LENGTH_SHORT).show()
+           }
+        }
     }
 }
---
+++ app/src/main/java/com/github/sahibmobdev/firebasetutorial/Person.kt
@@ -0,0 +1,7 @@
+package com.github.sahibmobdev.firebasetutorial
+
+data class Person(
+    var firstName: String,
+    var lastName: String,
+    var age: Int
+)
