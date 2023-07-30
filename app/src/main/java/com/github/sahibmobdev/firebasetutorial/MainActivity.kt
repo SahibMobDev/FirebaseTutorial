13d506b7d5f17854aeeeb616c2413215a71bf349
SahibMobDev <sahibmobdev@gmail.com>
2023-07-30 23:46:11 +0400

    Updating Data - Firebase Firestore

--- app/src/main/java/com/github/sahibmobdev/firebasetutorial/MainActivity.kt
+++ app/src/main/java/com/github/sahibmobdev/firebasetutorial/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.github.sahibmobdev.firebasetutorial.databinding.ActivityMainBinding
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
@@ -21,12 +22,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
         binding.btnUploadData.setOnClickListener {
             binding.apply {
-                val firstName = etFirstName.text.toString()
-                val lastName = etLastName.text.toString()
-                val age = etAge.text.toString().toInt()
-                val person = Person(firstName, lastName, age)
+                val person = oldPersonData()
                 savePerson(person)
             }
         }
@@ -35,7 +35,73 @@ class MainActivity : AppCompatActivity() {
             retrievePersons()
         }
 
+        binding.btnUpdatePerson.setOnClickListener {
+            val oldPerson = oldPersonData()
+            val newPerson = newPersonData()
+            setNewPersonData(oldPerson, newPerson)
+        }
+
     }
+
+    private fun oldPersonData(): Person {
+        binding.apply {
+            val firstName = etFirstName.text.toString()
+            val lastName = etLastName.text.toString()
+            val age = etAge.text.toString().toInt()
+            return Person(firstName, lastName, age)
+
+        }
+    }
+
+    private fun newPersonData(): Map<String, Any> {
+        binding.apply {
+            val newFirstName = etNewFirstName.text.toString()
+            val newLastName = etNewLastName.text.toString()
+            val newAge = etNewAge.text.toString()
+            val map = mutableMapOf<String, Any>()
+            if (newFirstName.isNotEmpty()) {
+                map["firstName"] = newFirstName
+            }
+            if (newLastName.isNotEmpty()) {
+                map["lastName"] = newLastName
+            }
+            if (newAge.isNotEmpty()) {
+                map["age"] = newAge
+            }
+            return map
+        }
+    }
+
+    private fun setNewPersonData(person: Person, newPerson: Map<String, Any>) = CoroutineScope(Dispatchers.IO).launch {
+        val personQuery = personCollectionRef
+            .whereEqualTo("firstName", person.firstName)
+            .whereEqualTo("lastName", person.lastName)
+            .whereEqualTo("age", person.age)
+            .get()
+            .await()
+
+        if (personQuery.documents.isNotEmpty()) {
+            for (document in personQuery) {
+                try {
+                    personCollectionRef.document(document.id).set(
+                        newPerson,
+                        SetOptions.merge()
+                    ).await()
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+        } else {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, "No person matched the query", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
 
     private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
         try {
@@ -58,7 +124,8 @@ class MainActivity : AppCompatActivity() {
                 .whereGreaterThan("age", fromAge)
                 .whereLessThan("age", toAge)
                 .orderBy("age")
-                .get().await()
+                .get()
+                .await()
             val sb = StringBuilder()
             for (document in querySnapshot) {
                 val person = document.toObject<Person>()
